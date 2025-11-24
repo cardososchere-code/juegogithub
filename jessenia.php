d08750b4ba292c7ac84d2c98cb39fbcb7e0bdb36
copacartagenajessenia-beep <copacartagenajessenia@pedropoveda.edu.bo>
2025-11-24 14:30:49 -0400

    jessicop

--- jessenia.php
+++ jessenia.php
@@ -1,3 +1,15 @@
-<?php
-echo"pepe"
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>TAREA-BTH</h1>
+    <h2>Sintaxis de Java</h2>
+    <p>Java es un lenguaje de programacion que esta basada en la creacion de objetos que tiene una esructura organizada y clara</p>
+    <p><br>su sintaxis es usar las llaves ({}),el punto y coma(;) estos serian los comandos o reglas que se debe utilizar para que se realice</p>
+    <h2>Estructuras repetitivas </h2>
+</body>
+</html>
