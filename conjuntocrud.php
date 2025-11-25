faf0eb728b18727b445032b6a08ebabdc2b3c42f
nahiaAS <agredasancheznahia@pedropoveda.edu.bo>
2025-11-25 11:29:41 -0400

    Update conjuntocrud.php

---
+++ conjuntocrud.php
@@ -0,0 +1,9 @@
+<?php
+echo "<h1>CRUD</h1>";
+echo "<h2?>Que es Crud?</h2>";
+echo "<h3?>Crud es un conjunto de cuatro operaciones que se realizan en cualquier base de datos:
+      C=Create(crear, permite agregar registros nuevos a la base de datos),
+      R=Read(leer, sirve para consultar o mostrar infromacion que ya es almacenada), 
+      U=Update(actualizar, te deja modificr datos existentes),
+      D=Delete(eliminar, se usa para borrar infromacion que ya no sirve)</h3>";
+?>
