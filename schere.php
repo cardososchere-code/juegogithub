01bab85c7e7a06f159d0644238058ce8a2d8689c
SchereC25 <cardosofloresschrezade@pedropoveda.edu.bo>
2025-11-25 11:31:29 -0400

    scheree

--- schere.php
+++ schere.php
@@ -1,3 +1,27 @@
-<?php
-echo"holaaaa"
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h3{
+            background-color: cyan;
+        }
+    </style>
+</head>
+<body>
+    <center><h1>LO QUE RECUERDO EN JAVA</h1></center>
+    <h3>Sintaxis java</h3>
+    <P>Java es un lenguaje de programación muy estructurado y ordenado. Usa clases como base de todo programa y cada archivo debe tener una clase principal. Es fuertemente tipado, lo que significa que siempre se debe indicar el tipo de dato de cada variable. Su sintaxis utiliza llaves para organizar bloques y punto y coma para terminar instrucciones. Es un lenguaje que requiere precisión y claridad al escribir.</P>
+    <h3>Estructuras repetitivas</h3>
+    <p>Las estructuras repetitivas en Java permiten ejecutar una acción varias veces. Las más comunes son for, while y do-while. Se usan cuando se necesita repetir un proceso, recorrer elementos o realizar tareas hasta que se cumpla una condición. Cada una funciona bajo una condición que determina cuántas veces se repite el bloque de instrucciones.</p>
+    <h3>POO(Atributos y Metodos)</h3>
+    <p>Java utiliza la Programación Orientada a Objetos. En este enfoque, una clase representa un modelo o plantilla, y un objeto es una instancia de esa clase. <br>
+       Los atributos son las características o datos que tiene un objeto. <br>
+       Los métodos son las acciones o comportamientos que puede realizar ese objeto. <br>
+       Este sistema permite organizar mejor el código, hacerlo reutilizable y mantener una estructura clara.</p>
+       <h3>Herencia</h3>
+       <p>La herencia es un concepto de la POO que permite que una clase pueda extender otra clase y recibir sus atributos y métodos. Esto ayuda a evitar repetir código y permite crear jerarquías de objetos. Una clase “hija” puede ampliar o modificar el comportamiento de la clase “padre”. Es una de las bases más importantes de Java para construir programas grandes y organizados.</p>
+</body>
+</html>
