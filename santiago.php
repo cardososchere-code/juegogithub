fb4985bd097d678ae46520356e1377d49121bb6b
asielEsc <riveraaraucosantiago@pedropoveda.edu.bo>
2025-11-25 12:10:47 -0400

    santiago

--- santiago.php
+++ santiago.php
@@ -1,3 +1,11 @@
 <?php
-echo"sdzfsdf"
+echo"sdzfsdf"ç
+echo"<h1>Sintaxis de Jva</h1>";
+echo"<p>La sintaxis de Java es el conjunto de reglas que definen cómo se escribe y organiza el código en este lenguaje</p>";
+echo"<h1>Estructuras Repetitivas</h1>";
+echo"<p>En Java, las estructuras repetitivas también llamadas bucles permiten ejecutar un bloque de código varias veces mientras se cumpla una condición.</p>";
+echo"<h1>Atributos y Metodos</h1>";
+echo"<p>Los atributos son las propiedades o variables que describen el estado de un objeto y los metodos son las propiedades o variables que describen el estado de un objeto.</p>";
+echo"<h1>Herencia</h1>";
+echo"<p>La herencia en Java es un mecanismo que permite que una clase reutilice atributos y métodos de otra clase.</p>";
 ?>
